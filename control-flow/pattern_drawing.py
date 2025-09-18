112db0c445025e09ff50f9892b45abd8e0be6113
timothy kemboi <tkemboi889@gmail.com>
2025-09-18 14:09:21 +0300

    add the file pattern

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+pattern=int(input("enter the the size of the pattern:"))
+rows=4
+i=1
+while i<=rows:
+    j=1
+    while j<=rows +1:
+        print("*"  ,end=" ")
+        j+=1
+        
+        print()
+        i+=1
