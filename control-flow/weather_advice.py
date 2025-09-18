742f873f80f12217ab8c7308ce47ba9688ac248c
timothy kemboi <tkemboi889@gmail.com>
2025-09-18 13:46:11 +0300

    add control statent to the file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,7 @@
 the_weather= input("what's the weather like today? (sunny/rainy/cold):")
 if the_weather=="sunny":
     print("wear a t-shirt and sunglasses:")
-elif the_weather=="rainny":
+elif the_weather=="rainy":
     print("don't forget umbrella and raincoat")
 elif the_weather=="cold":
     print("make sure to wear a coat and scarf:")
