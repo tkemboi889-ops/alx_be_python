f387d163f544f4c61805d27bfd8c3b41e715feb9
timothy kemboi <tkemboi889@gmail.com>
2025-09-18 10:08:32 +0300

    add the if ,elif and else statement to the file

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+user= input("what is the weather like today? sunny/rainy/cold")
+if user=="sunny":
+    print("wear a t-shirt and sunglasses:")
+elif user=="rainny":
+    print("don't forget umbrella and raincoat")
+elif user=="cold":
+    print("make sure to wear a coat and scarf:")
+else:
+    print("sorry,i don't have recommendations for this weather:")
+    
