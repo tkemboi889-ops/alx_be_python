4cc69eb0879e34c74d217f30498239488119ae76
timothy kemboi <tkemboi889@gmail.com>
2025-09-18 19:20:45 +0300

    add control flow to the file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-the_weather= input("what's the weather like today? (sunny/rainy/cold):")
-if the_weather=="sunny":
-    print("wear a t-shirt and sunglasses:")
-elif the_weather=="rainy":
-    print("don't forget umbrella and raincoat")
-elif the_weather=="cold":
-    print("make sure to wear a coat and scarf:")
+weather= input("What's the weather like today? (sunny/rainy/cold):").lower()
+if weather == "sunny":
+    print("wear a t-shirt and sunglasses.")
+elif weather== "rainy":
+    print("don't forget umbrella and raincoat.")
+elif weather == "cold":
+    print("make sure to wear a coat and scarf.")
 else:
-    print("sorry,i don't have recommendations for this weather:")
+    print("sorry,i don't have recommendations for this weather.")
