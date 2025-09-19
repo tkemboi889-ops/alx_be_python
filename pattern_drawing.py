8ee7b567f937d7e36022c8c49aff1369a43531ee
timothy kemboi <tkemboi889@gmail.com>
2025-09-19 16:44:40 +0300

    initial changes are being made

---
+++ pattern_drawing.py
@@ -0,0 +1,10 @@
+user_input=int(input("Enter the  size of the pattern:"))
+rows=0
+while rows<=user_input:
+    column=0
+    for column in range(user_input):
+         print("*" , end="")
+         column+=1
+        
+    print()
+    rows+=1
