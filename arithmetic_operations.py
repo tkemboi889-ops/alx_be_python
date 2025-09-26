0d1552bd29a96b06a446dbe2efb2d26efd92765d
timothy kemboi <tkemboi889@gmail.com>
2025-09-26 09:00:14 +0300

    add arithmetic operations

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -1,4 +1,7 @@
-def perform_operation(num1 , num2 , operation ):
+def perform_operation(num1, num2, operation):
+    num1 =float()
+    num2 =float()
+    operation = str("add,subtract,multiply,divide")
     
     if operation == "add":
         result= num1 + num2
