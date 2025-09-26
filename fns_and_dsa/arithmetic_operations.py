40a733ad7f911f452a9fcd66544204717b2e7cc1
timothy kemboi <tkemboi889@gmail.com>
2025-09-26 09:08:07 +0300

    add initial commit of a new project

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1 , num2 , operation ):
+    
+    if operation == "add":
+        result= num1 + num2
+        return result
+    elif operation == "subtract":
+        result= num1 - num2
+        return result
+    elif operation == "multiply":
+        result= num1 * num2
+        return result
+    elif operation == "divide":
+        if num2 == 0:
+            print( "Error: Division by zero")
+        else:
+            return num1 / num2
+    return result
